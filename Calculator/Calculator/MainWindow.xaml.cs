e573342151e0aeb0cdd90577d944a0061a9a9054
Miguel-124 <michalgorecki124@gmail.com>
2022-12-14 23:24:56 +0100

    Add project files.

---
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ResultText.Text= string.Empty;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ResultText.Text = string.Empty;
+            var button = sender as Button;
+            var currentNumber = button.Name[button.Name.Length-1];
+            CurrentOperationText.Text+= currentNumber;
+        }
+
+        
+        private void ButonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+            }
+
+            CurrentOperationText.Text += "+";
+        }
+
+        private void ButonMinus_Click(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+            }
+            CurrentOperationText.Text += "-";
+        }
+
+        private void ButonMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+            }
+            CurrentOperationText.Text += "*";
+        }
+
+        private void ButonDivide_Click(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+            if (ContainsOperation(operation))
+            {
+                CurrentOperationText.Text = CalculateResult(operation).ToString();
+            }
+            CurrentOperationText.Text += "/";
+        }
+
+        private void ButonResult_Click(object sender, RoutedEventArgs e)
+        {
+            var operation = CurrentOperationText.Text;
+
+
+            ResultText.Text = CalculateResult(operation).ToString();
+
+            CurrentOperationText.Text = string.Empty;
+
+        }
+        private bool ContainsOperation(string operation)
+            => operation.Contains("-") || operation.Contains("+") || operation.Contains("*") || operation.Contains("/");
+        
+        private int CalculateResult(string operation)
+        {
+            if (operation.Contains("+"))
+            {
+                var addElements = operation.Split('+');
+                return int.Parse(addElements[0]) + int.Parse(addElements[1]);
+            }
+            if (operation.Contains("-"))
+            {
+                var addElements = operation.Split('-');
+                return int.Parse(addElements[0]) - int.Parse(addElements[1]);
+            }
+            if (operation.Contains("*"))
+            {
+                var addElements = operation.Split('*');
+                return int.Parse(addElements[0]) * int.Parse(addElements[1]);
+            }
+            if (operation.Contains("/"))
+            {
+                var addElements = operation.Split('/');
+                return int.Parse(addElements[0]) / int.Parse(addElements[1]);
+            }
+            return default;
+        }
+    }
+}
---
+++ TicTacToe/TicTacToe/MainWindow.xaml.cs
@@ -0,0 +1,133 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+//ctr+r+g usuwa niepotrzebne rzeczy
+
+namespace TicTacToe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int counter;
+        public bool IsPlayer1Turn { get; set; } = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+            NewGame();
+        }
+
+        public void NewGame()
+        {
+            IsPlayer1Turn = false;
+            counter = 0;
+            Button_0_0.Content = string.Empty;
+            Button_0_1.Content = string.Empty;
+            Button_0_2.Content = string.Empty;
+            Button_1_0.Content = string.Empty;
+            Button_1_1.Content = string.Empty;
+            Button_1_2.Content = string.Empty;
+            Button_2_0.Content = string.Empty;
+            Button_2_1.Content = string.Empty;
+            Button_2_2.Content = string.Empty;
+
+            Button_0_0.Background = Brushes.White;
+            Button_0_1.Background = Brushes.White;
+            Button_0_2.Background = Brushes.White;
+            Button_1_0.Background = Brushes.White;
+            Button_1_1.Background = Brushes.White;
+            Button_1_2.Background = Brushes.White;
+            Button_2_0.Background = Brushes.White;
+            Button_2_1.Background = Brushes.White;
+            Button_2_2.Background = Brushes.White;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            IsPlayer1Turn ^= true;
+            counter++;
+            if (counter > 9)
+            {
+                NewGame();
+                return;
+            }
+            var button = sender as Button;
+            button.Content = IsPlayer1Turn ? "O" : "X";
+
+            if (CheckIfPlayerWon())
+            {
+                counter = 9;
+            }
+        }
+        private bool CheckIfPlayerWon()
+        {
+            //Column0
+            if (Button_0_0.Content != string.Empty && Button_0_0.Content == Button_0_1.Content && Button_0_1.Content == Button_0_2.Content)
+            {
+                Button_0_0.Background = Brushes.Green;
+                Button_0_1.Background = Brushes.Green;
+                Button_0_2.Background = Brushes.Green;
+                return true;
+            }
+            //Column1
+            if (Button_1_0.Content != string.Empty && Button_1_0.Content == Button_1_1.Content && Button_1_1.Content == Button_1_2.Content)
+            {
+                Button_1_0.Background = Brushes.Green;
+                Button_1_1.Background = Brushes.Green;
+                Button_1_2.Background = Brushes.Green;
+                return true;
+            }
+            //Column2
+            if (Button_2_0.Content != string.Empty && Button_2_0.Content == Button_2_1.Content && Button_2_1.Content == Button_2_2.Content)
+            {
+                Button_2_0.Background = Brushes.Green;
+                Button_2_1.Background = Brushes.Green;
+                Button_2_2.Background = Brushes.Green;
+                return true;
+            }
+
+            // Row0
+            if (Button_0_0.Content != string.Empty && Button_0_0.Content == Button_1_0.Content && Button_1_0.Content == Button_2_0.Content)
+            {
+                Button_0_0.Background = Brushes.Green;
+                Button_1_0.Background = Brushes.Green;
+                Button_2_0.Background = Brushes.Green;
+                return true;
+            }
+            // Row1
+            if (Button_0_1.Content != string.Empty && Button_0_1.Content == Button_1_1.Content && Button_1_1.Content == Button_2_1.Content)
+            {
+                Button_0_1.Background = Brushes.Green;
+                Button_1_1.Background = Brushes.Green;
+                Button_2_1.Background = Brushes.Green;
+                return true;
+            }
+            // Row2
+            if (Button_0_2.Content != string.Empty && Button_0_2.Content == Button_1_2.Content && Button_1_2.Content == Button_2_2.Content)
+            {
+                Button_0_2.Background = Brushes.Green;
+                Button_1_2.Background = Brushes.Green;
+                Button_2_2.Background = Brushes.Green;
+                return true;
+            }
+
+            //
+            if (Button_0_0.Content != string.Empty && Button_0_0.Content == Button_1_1.Content && Button_1_1.Content == Button_2_2.Content)
+            {
+                Button_0_0.Background = Brushes.Green;
+                Button_1_1.Background = Brushes.Green;
+                Button_2_2.Background = Brushes.Green;
+                return true;
+            }
+            if (Button_0_2.Content != string.Empty && Button_0_2.Content == Button_1_1.Content && Button_1_1.Content == Button_2_0.Content)
+            {
+                Button_2_2.Background = Brushes.Green;
+                Button_1_1.Background = Brushes.Green;
+                Button_2_0.Background = Brushes.Green;
+                return true;
+            }
+            return false;
+        }
+    }
+}
